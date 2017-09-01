adb6cbf8852b01b92d93370508e9417cf0520f3a
David Moore <davidimoore@gmail.com>
2017-09-01 11:02:39 -0700

    add user resources with styled views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+
+20.times do
+  first_name =  Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  User.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: Faker::Internet.email("#{first_name} #{last_name}"),
+    date_of_birth: Faker::Date.birthday(18, 65)
+  )
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    first_name "MyString"
+    last_name "MyString"
+    date_of_birth "2017-09-01 10:18:36"
+    email "MyString"
+  end
+end
